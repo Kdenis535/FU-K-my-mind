68dd6bacf913edf97a186336abfe914d609f7584
Kdenis535 <kdenis535@gmail.com>
2018-04-09 17:31:21 +0300

    допилил все кторме адаптива

--- www/js/init.js
+++ www/js/init.js
@@ -1,4 +1,11 @@
 $(function() {
     $( "#services" ).tabs();
     $( "#portfolio" ).tabs();
+
+    //Init slick carousel 
+    $('.reviews-carousel').slick({
+      dots: true,
+      prevArrow: '<div class="slick-prev slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-left"></i></div></div>', 
+      nextArrow: '<div class="slick-next slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-right"></i></div></div>', 
+    });
 });
